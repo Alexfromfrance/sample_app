28e4016ceb5b297a7329d2f6145fe905b0c9f091
alexandre auriol <auriol.alexandre@gmail.com>
2019-10-01 15:40:47 +0200

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!( name:     "Example User",
-              email:    "example@railstutorial.org",
+              email:    "exemple@railstutorial.org",
               password: "foobar",
               password_confirmation: "foobar",
               admin: true,
@@ -28,7 +28,15 @@ end
 
 users = User.order(:created_at).take(6)
 50.times do
-  content = Faker::Lorem.sentence(5)
+  content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each {|followed| user.follow(followed)}
+followers.each {|follower| follower.follow(user)}
+
 

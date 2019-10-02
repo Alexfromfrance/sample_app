d4ae6bbc5587dce4c48d643aa17d0e312273f1dc
alexandre auriol <auriol.alexandre@gmail.com>
2019-10-02 14:49:39 +0200

    config cloudinary

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -9,17 +9,13 @@ class PictureUploader < CarrierWave::Uploader::Base
   # storage :file
   # storage :fog
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
+
+
 
   def extension_whitelist
     %w(jpg jpeg gif png)
